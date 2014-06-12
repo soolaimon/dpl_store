f6eeb435251a3a78edc2ba62c3bb640a914f1781
Dave Nelson <davidicus21@gmail.com>
2014-06-12 14:22:00 -0600

    Added new product page, action, create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,37 @@ class ProductsController < ApplicationController
    # This should list all of our products
    # index.html.erb
   def index
+    @products = Product.all
+  end
 
+  # This should find a particular product
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+              # No using .create because that would save to DB, but we don't have anything to save to DB yet.
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save # returns boolean
+      redirect_to products_path
+    else
+      # Saving to DB failed
+      # Do something else
+      render :new
+    end
+  end
+
+  private
+  # define a private method
+  # use strong parameters
+
+  def product_params
+    # params[:product]
+    params.require(:product).permit(:name)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+
+  validates :name, presence: true # extra layer of validation. null: false doesn't accoun for empty strings.
+
 end

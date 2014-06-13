5756637216e2867451456b4b3923bf7184e35630
Dave Nelson <davidicus21@gmail.com>
2014-06-13 12:05:02 -0600

    forgot to commit. added lots of stuf. i suck

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
    # The index action
    # This should list all of our products
    # index.html.erb
@@ -8,32 +9,55 @@ class ProductsController < ApplicationController
 
   # This should find a particular product
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
               # No using .create because that would save to DB, but we don't have anything to save to DB yet.
     @product = Product.new
+
   end
 
   def create
+
     @product = Product.new(product_params)
     if @product.save # returns boolean
+      flash[:notice] = 'Good job! You did it.'
       redirect_to products_path
     else
       # Saving to DB failed
       # Do something else
+      flash[:alert] = 'Bad job! You failed.'
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @product.update_attributes(product_params) # returns boolean depending on whether it updated or not
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
   # define a private method
   # use strong parameters
-
   def product_params
     # params[:product]
     params.require(:product).permit(:name)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 
 end
